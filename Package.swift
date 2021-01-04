9e546b61eed131bdbd54c0817af09a29c875baf8
Dipanjan Jana <dipanjan.jana@smallcase.com>
2021-01-04 19:32:20 +0530

    version 2.2.0

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SCGatewayPackage",
+    name: "SCGateway",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SCGatewayPackage",
-            targets: ["SCGatewayPackage"]),
+            name: "SCGateway",
+            targets: ["SCGateway"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,12 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SCGatewayPackage",
-            dependencies: []),
-        .testTarget(
-            name: "SCGatewayPackageTests",
-            dependencies: ["SCGatewayPackage"]),
         .binaryTarget(name: "SCGateway",
                       url: "https://gateway.smallcase.com/scdk_ios_xcode_12/2.2.0/SCGateway.xcframework.zip",
                       checksum: "ff1c962fe08ebc3ef8f86428c75a0c15e83e1d867397cb0a05d386f93c60abc7"),
